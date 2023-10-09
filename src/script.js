309175220830ad76896d0a36fdc25c167966bf8d
Mshahzad56 <m.shahzad.mail.56@gmail.com>
2023-10-09 20:12:26 +0500

    main

---
+++ src/script.js
@@ -0,0 +1,11 @@
+const likeBtn = document.querySelector(".like-btn");
+
+likeBtn.addEventListener('click', function() {
+  if (likeBtn.classList.contains("active")) {
+    likeBtn.classList.remove("active");
+  } else {
+    likeBtn.classList.add("active");
+  }
+  })
+
+
